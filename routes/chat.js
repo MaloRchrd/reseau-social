4ce5a488860ed51d1860a3c59bc4d1a82b783ef0
Malo <malo.rchrd+git@gmail.com>
2017-09-05 12:55:45 +0200

    last try socket io

--- routes/chat.js
+++ routes/chat.js
@@ -14,7 +14,7 @@ var User = require('../app/models/user');
 // var io = require('socket.io').listen(server);  //pass a http.Server instance
 // server.listen(80);  //listen on port 80
 
-var server = app.listen(3000);
+var server = app.listen(7777);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
 var fs = require('fs');

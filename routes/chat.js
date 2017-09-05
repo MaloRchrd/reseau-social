c13e4f6fb8c8c5b37c295e23572ff4e1dfc71b2d
Malo <malo.rchrd+git@gmail.com>
2017-09-05 12:49:35 +0200

    socket io

--- routes/chat.js
+++ routes/chat.js
@@ -14,7 +14,7 @@ var User = require('../app/models/user');
 // var io = require('socket.io').listen(server);  //pass a http.Server instance
 // server.listen(80);  //listen on port 80
 
-var server = app.listen(5000);
+var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
 var fs = require('fs');
